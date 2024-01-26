98e5e8b04fac3460bf99a88213bfae81733bb52c
kuysi <37512301+Shukmanov@users.noreply.github.com>
2024-01-26 18:12:02 +0600

    Update string_ex.py

--- exercisess/string_ex.py
+++ exercisess/string_ex.py
@@ -1,1 +1,81 @@
 
+print("Hello world")
+print('zaWarudoooooo')
+words_music = "     kororon naaaaa kimi dakaraaaa   "
+
+print(words_music)
+
+words_music_but = """pa
+shel
+v
+jopu"""
+
+print(words_music_but)
+# arrays
+
+print(words_music[1], words_music[4])
+
+# if for
+
+z = "434"
+for x in "jfka":
+    for y in "uoeu":
+        print(x,
+    y,
+    z,
+    len(words_music),
+    "kimi" in words_music
+    )
+    
+if z in words_music:
+    print("it has")
+    
+# from one to another
+print(words_music[12:-10])
+
+print(words_music[3:])
+
+print(words_music[-12:-7])
+
+#split upper lower strip replace
+print    (words_music.upper())
+
+print   (words_music
+.upper()
+.lower())
+
+print           (words_music
+.lower())
+
+print(words_music.strip())
+
+print(words_music.replace("a", "you"))
+
+print(words_music.split())
+
+words_music = words_music.upper()
+
+print(words_music)
+
+#concatination
+
+print(words_music.strip().lower() + z)
+
+# formating stringssssss arigatooo gozaimaassssss azaaassss
+name2 = "DIO"
+technique = "ZAWARUDO"
+anime_ex = "Kono {} daaaaa. {}!"
+
+print(anime_ex.format( name2, technique))
+
+STANDO = "requiem"
+name3 = "Joruno"
+surname = "Jowanna"
+anime_ex2 = "Kore wa {2}! Kono {0} {1} yume ga aru!" #Kore wa REQUIEM! Kono JORUNO JOWANNA yume ga aru!
+print(anime_ex2.format(STANDO, name3, surname))
+ # double ""
+print("We are the so-called \"Vikings\" from the north.")
+
+
+
+
