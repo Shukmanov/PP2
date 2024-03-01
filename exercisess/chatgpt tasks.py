4f53dfc88373ed4220ac61c78a616aa10d8e5997
kuysi <kuanish.shukmanov@gmail.com>
2024-03-01 15:06:25 +0600

    Create chatgpt tasks.py

---
+++ exercisess/chatgpt tasks.py
@@ -0,0 +1,47 @@
+# fist one
+
+given_number = 1
+
+while(given_number < 10):
+    print(given_number)
+    given_number += 1
+
+#second one
+
+shopping_list = ["banan", "food", "chicken", "pen", "is"]
+
+for products in shopping_list:
+    print(products)
+
+#third one
+
+massive = [1, 3, 6, 9, 94, 34,95, 738]
+
+for numbers in massive:
+    print(numbers)
+
+
+#forth one
+
+day_of_week = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+print(day_of_week[3])
+
+
+#fifth one
+
+set1 = {1, 2, 5}
+set2 = {3, 4}
+
+for number in set2:
+    set1.add(number)
+
+print(set1)
+
+#sixth one
+
+children_info = {"name1": 83, "name2": 38, "name3": 23, "name4": 12}
+
+for names in children_info:
+    print(children_info[names])
+
